1ed820fdbd3b76a3b36406f261baca9d272ceaa0
Leonardo Moretti <leonardonabarro2003@gmail.com>
2024-02-05 14:51:36 -0300

    test: testando componente PostComments

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -2,9 +2,42 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import Post from '.';
 import PostComment from '.';
 
+const comments = [
+    {
+        id: 1,
+        comment: 'teste1'
+    },
+    {
+        id: 2,
+        comment: 'teste2'
+    }
+]
+
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve renderizar dois comentários', () => {
+        render(<PostComment />)
+
+        //adicionando primeiro comentario
+        fireEvent.change(screen.getByTestId('value-comment'), {
+            target: {
+                value: 'Comentario de teste'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comment'))
+
+        //adicionando segundo commentario
+        fireEvent.change(screen.getByTestId('value-comment'), {
+            target: {
+                value: 'Segundo comentário de teste'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comment'))
+
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2)
+    })
 });
